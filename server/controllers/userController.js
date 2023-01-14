976fb792375793cff2219f0570ca44cd1b0c7aa5
Kevin <kevin.06.huang@gmail.com>
2023-01-14 10:57:40 -0800

    Destructured user password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,14 @@
 const userController = {};
 
 userController.verifyUser = async (req, res, next) => {
+  const { username, password } = req.body;
+
   return next();
 };
 
 userController.createUser = async (req, res, next) => {
+  const { username, password } = req.body;
+
   return next();
 }
 
