c1186ee61d1975b32440bc99f7b23a92539d0f58
Kevin <kevin.06.huang@gmail.com>
2023-01-16 22:15:00 -0800

    minor refactor

--- client/App.jsx
+++ client/App.jsx
@@ -13,26 +13,26 @@ import Protected from './components/auth/Protected';
 import Authapp from './components/Authapp.jsx';
 
 const App = () => {
-    let id = null;
+  let id = null;
 
-    const token = Cookies.get('token');
-    if (token) {
-      const payload = jwt_decode(token);
-      id = payload.id;
-    };
+  const token = Cookies.get('token');
+  if (token) {
+    const payload = jwt_decode(token);
+    id = payload.id;
+  };
 
-    return (
-      <AuthProvider>
-        <Routes>
-          <Route exact path="/login" element={<Login/>}/>
-          <Route exact path="/signup" element={<Signup/>}/>
-          <Route exact path="/" element={
-              <Protected userId={id}>
-                <Authapp/>
-              </Protected>}/>
-            </Routes>
-      </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/signup" element={<Signup/>}/>
+        <Route exact path="/" element={
+            <Protected userId={id}>
+              <Authapp/>
+            </Protected>}/>
+          </Routes>
+    </AuthProvider>
+  );
 } 
 
 export default App;
