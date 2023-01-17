ea7e2e4bb02df7a2bd2534a571fb166c276e1bad
Kevin <kevin.06.huang@gmail.com>
2023-01-16 22:10:17 -0800

    Refactored, hooked up final Char

--- client/components/Authapp.jsx
+++ client/components/Authapp.jsx
@@ -1,42 +1,39 @@
 import React, {useState}  from 'react';
+
 import Home from '../pages/Home';
 import Race from '../pages/Race';
 import CharClass from '../pages/CharClass';
 import Alignment from '../pages/Alignment';
+import FinalChar from '../pages/FinalChar';
+
 const Authapp = () => {
-    const [race, setRace] = useState('');
-    const [charClass, setCharClass] = useState('');
-    const [alignment, setAlignment] = useState('');
-    const [currentComponent, setCurrentComponent] = useState('home');
+  const [race, setRace] = useState('');
+  const [charClass, setCharClass] = useState('');
+  const [alignment, setAlignment] = useState('');
+  const [currentComponent, setCurrentComponent] = useState('home');
 
-    const dndNames = ['Regdar', 'Todrdek', 'Alhandra', 'Eberk', 'Athain', 'Arthon', 'Kazak', 'Ragnara', 'Sandharrow', 'Morthos',]
+  const dndNames = ['Regdar', 'Todrdek', 'Alhandra', 'Eberk', 'Athain', 'Arthon', 'Kazak', 'Ragnara', 'Sandharrow', 'Morthos',]
 
-    const switchComponent = (currentComponent, setCurrentComponent) => {
-       
-        switch(currentComponent) {
-            case "home":
-              // code block
-              
-              return (<Home setCurrentComponent={setCurrentComponent}/>)
-            case "race":
-              // code block
-              return (<Race setCurrentComponent={setCurrentComponent} setRace={setRace}/>)
-            case "charclass":
-              // code block
-              return (<CharClass setCurrentComponent={setCurrentComponent} setCharClass={setCharClass}/>);
-            case "alignment":
-                // code block
-                return (<Alignment setCurrentComponent={setCurrentComponent} setAlignment={setAlignment}/>);
-            default:
-              // code block
-          }
-    }
+  const switchComponent = (currentComponent, setCurrentComponent) => {
+    switch(currentComponent) {
+      case "home":
+        return (<Home setCurrentComponent={setCurrentComponent}/>)
+      case "race":
+        return (<Race setCurrentComponent={setCurrentComponent} setRace={setRace}/>)
+      case "charclass":
+        return (<CharClass setCurrentComponent={setCurrentComponent} setCharClass={setCharClass}/>);
+      case "alignment":
+        return (<Alignment setCurrentComponent={setCurrentComponent} setAlignment={setAlignment}/>);
+      case "finalChar":
+        return (<FinalChar/>);
+    };
+  };
 
-    return (
-        <div>  
-            {switchComponent(currentComponent, setCurrentComponent)}
-        </div>
-    );
+  return (
+    <div>  
+      {switchComponent(currentComponent, setCurrentComponent)}
+    </div>
+  );
 }
 
 export default Authapp;
